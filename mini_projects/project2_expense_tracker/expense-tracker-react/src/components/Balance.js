60a618dff4fe5f4d012195a479cb8d2ab4597ce1
iamyunotyou <yuthd3@gmail.com>
2021-03-30 23:06:49 +0900

    context js

--- mini_projects/project2_expense_tracker/expense-tracker-react/src/components/Balance.js
+++ mini_projects/project2_expense_tracker/expense-tracker-react/src/components/Balance.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export const Balance = () => {
     return (
-        <>
+        <div>
             <h4>Your Balance</h4>
-            <h1 id="balance">
+            <h1>
                 $0.00
             </h1>
-        </>
+        </div>
     )
 }
