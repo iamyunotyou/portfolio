cb900c9c38b6631c92df3492f95aca0a375da819
iamyunotyou <yuthd3@gmail.com>
2021-03-30 23:33:09 +0900

    adjust transaction.js

--- mini_projects/project2_expense_tracker/expense-tracker-react/src/components/Transaction.js
+++ mini_projects/project2_expense_tracker/expense-tracker-react/src/components/Transaction.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Transaction = () => {
+export const Transaction = ({ transaction }) => {
     return (
         <li className="minus">
         {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
